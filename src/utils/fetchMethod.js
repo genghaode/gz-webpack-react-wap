2ae4ef784e15e1594ffcde98d0e24631c3469ed6
genghaode <genghaode@126.com>
2017-03-07 17:08:01 +0800

    去掉多余的

--- src/utils/fetchMethod.js
+++ src/utils/fetchMethod.js
@@ -1,17 +1,9 @@
 import axios from 'axios'
 
-let url = ''
-if (__DEV__) {
-  url = 'http://localhost:9090'
-}
-if (__PROD__) {
-  url = 'http://localhost:3000'
-}
-
 export const fetchTest = () => {
-  return axios.get(`${url}/api/test`)
+  return axios.get('http://localhost:3000/api/test')
 }
 
 export const fetchBanner = () => {
-  return axios.get(`${url}/api/getBanner`)
+  return axios.get('http://localhost:3000/api/getBanner')
 }
